e8f9adc616639f41579c653ab34680ee2e843c84
Onur Ozan sünger <onurozansunger@gmail.com>
2022-11-02 15:24:26 +0300

    .

---
+++ src/test/java/com/example/muhabbet_kusu/MuhabbetKusuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muhabbet_kusu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuhabbetKusuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
